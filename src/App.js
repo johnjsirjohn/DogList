60dc1ac692a9f300036b6b526c6e1b818bd5413b
johnjsirjohn <ionut.sirbu@hotmail.com>
2019-06-13 12:11:18 +0200

    Adding the superagent Component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import superagent from 'superagent';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header' />
+      </div>
+    );
+  }
+}
+export default App;
